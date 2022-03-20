2d868ec59bc7c54359c15551ec3ff2d504b132bb
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-03-20 14:34:48 +0900

    アプリ画面作成-header,main

---
+++ src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-page">
+        <div class="left-menu">
+            左
+        </div>
+        <div class="right-menu">
+            右
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.main-page {
+  display: flex;
+  height: calc(100vh - 60px);
+  .left-menu {
+    width: 350px;
+    background-color: #f7f6f3;
+  }
+  .right-view {
+    flex-grow: 1;
+    padding: 10px;
+  }
+}
+</style>
